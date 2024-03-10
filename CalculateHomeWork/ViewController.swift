bada929a11c83d2d418f3c6ffb1acad4829146ef
Hakan Hardal <hakanhardal@icloud.com>
2024-03-10 20:37:18 +0300

    Initial Commit

---
+++ CalculateHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculateHomeWork
+//
+//  Created by Hakan Hardal on 10.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
